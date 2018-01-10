f9fd562c4d65c4330eda9f9a747530afb76f0719
Amiel Martin <amiel.martin@gmail.com>
2018-01-10 10:29:43 -0800

    Copy implementation, documentation, and specs from existing code

--- lib/action_sprout/method_object.rb
+++ lib/action_sprout/method_object.rb
@@ -1,7 +1,41 @@
-require "action_sprout/method_object/version"
+require 'action_sprout/method_object/version'
+require 'kwattr'
 
 module ActionSprout
+  # MethodObject is a quick shortcut for creating "method objects". Under the
+  # hood, it uses kwattr. Check out the [kwattr docs][] for how to configure
+  # options.
+  #
+  # [kwattr docs]: https://github.com/etiennebarrie/kwattr
+  #
+  # Example:
+  #
+  #   class SaveUser
+  #     method_object :user, repository: UserRepository.new
+  #
+  #     def call
+  #       repository.save user
+  #     end
+  #   end
+  #
+  #   SaveUser.call user: user
+  #
+  # Summary of kwattr options:
+  #
+  # kwattr stands for key-word-attributes and therefore only supports setting up
+  # method objects that take keyword arguments. Each positional argument passed
+  # to method_object represents a required attribute, and each keyword argument
+  # passed to method_object represents an option attribute and its default.
   module MethodObject
-    # Your code goes here...
+    def method_object(*args)
+      extend ClassMethods
+      kwattr(*args)
+    end
+
+    module ClassMethods
+      def call(**args, &block)
+        new(**args).call(&block)
+      end
+    end
   end
 end
--- spec/action_sprout/method_object_spec.rb
+++ spec/action_sprout/method_object_spec.rb
@@ -1,11 +1,62 @@
 require "spec_helper"
 
 RSpec.describe ActionSprout::MethodObject do
-  it "has a version number" do
+
+  it 'has a version number' do
     expect(ActionSprout::MethodObject::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  Module.include ActionSprout::MethodObject
+
+  class MyMethodObject
+    method_object
+
+    def call
+      'my-method-object-was-called'
+    end
+  end
+
+  class AMethodObjectWithOptions
+    method_object :foo, bar: 42
+
+    def call
+      [foo, bar]
+    end
+  end
+
+  class NoCallDefined
+    method_object
+  end
+
+  class WithABlock
+    method_object :foo
+
+    def call
+      yield foo
+    end
+  end
+
+  it 'can call the method object' do
+    result = MyMethodObject.call
+    expect(result).to eq 'my-method-object-was-called'
+  end
+
+  it 'can configure options' do
+    result = AMethodObjectWithOptions.call foo: Math::PI, bar: Math::E
+    expect(result).to eq [Math::PI, Math::E]
+  end
+
+  it 'can use default options' do
+    result = AMethodObjectWithOptions.call foo: Math::PI
+    expect(result).to eq [Math::PI, 42]
+  end
+
+  it 'raises a useful no method error when call is not defined' do
+    expect { NoCallDefined.call }.to raise_error(NoMethodError, /call.*NoCallDefined/)
+  end
+
+  it 'can pass a block' do
+    expect { |block| WithABlock.call(foo: :bar, &block) }.to yield_with_args(:bar)
   end
 end
+
